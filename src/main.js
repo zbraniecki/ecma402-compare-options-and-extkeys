416dbf50e2ae7689ad55143bd1029efc1cd13cb9
Zibi Braniecki <gandalf@mozilla.com>
2016-12-06 19:06:29 -1000

    Complete tests

--- src/main.js
+++ src/main.js
@@ -13,6 +13,58 @@
     return `${languageTag}-u-${extKeys.join('-')}`;
   }
 
+  function testFormatter(fmtName, Intl) {
+    if (!Intl.hasOwnProperty(fmtName)) {
+      return false;
+    }
+
+    const formatter = tests[fmtName];
+    const extKeys = formatter.extensionKeys;
+
+    const results = {
+      extensionKeys: {},
+      options: {}
+    };
+
+    for (const key in extKeys) {
+      const value = extKeys[key].values[0];
+      const name = extKeys[key].name;
+      const extKey = { [key]: value };
+      const dtf = new Intl[fmtName](
+        constructLocaleString('en-US', extKey)
+      );
+      results.extensionKeys[key] = dtf.resolvedOptions()[name] === value;
+    };
+
+    for (const name in formatter.options) {
+      const values = formatter.options[name].values;
+      const optionsBase = formatter.options[name].optionsBase || {};
+
+      const res = {};
+
+      for (const i in values) {
+        const val = values[i];
+        const options = Object.assign({
+          [name]: val
+        }, Array.isArray(optionsBase) ? optionsBase[i] : optionsBase);
+        const fmt = new Intl[fmtName]('en-US', options);
+        res[val] = fmt.resolvedOptions()[name] === val;
+      };
+
+      let allTrue = Object.keys(res).every(name => res[name]);
+      let allFalse = Object.keys(res).every(name => !res[name]);
+
+      if (allTrue) {
+        results.options[name] = true;
+      } else if (allFalse) {
+        results.options[name] = false;
+      } else {
+        results.options[name] = res;
+      }
+    }
+    return results;
+  }
+
   /* Tests */
 
   const tests = {
@@ -26,52 +78,53 @@
         'timeZone': {'values': ['Asia/Shanghai']},
         'hour12': {'values': [true, false], 'optionsBase': {'hour': '2-digit'}},
         'weekday': {'values': ['narrow', 'short', 'long'], 'optionsBase': {'day': '2-digit'}},
+        'era': {'values': ['narrow', 'short', 'long']},
+        'year': {'values': ['numeric', '2-digit']},
+        'month': {'values': ['numeric', '2-digit', 'narrow', 'short', 'long']},
+        'day': {'values': ['numeric', '2-digit']},
+        'hour': {'values': ['numeric', '2-digit'], 'optionsBase': [{}, {'hour12': false}]},
+        'minute': {'values': ['numeric', '2-digit'], 'optionsBase': [{}, {'hour': 'numeric'}]},
+        'second': {'values': ['numeric', '2-digit'], 'optionsBase': [{}, {'minute': '2-digit'}]},
+        'timeZoneName': {'values': ['short', 'long']},
+
         //'localeMatcher': {'values': ['lookup', 'best fit']},
         //'formatMatcher': {'values': ['basic', 'best fit']},
       },
-      'test': (Intl) => {
-        if (!Intl.hasOwnProperty("DateTimeFormat")) {
-          return false;
-        }
+    },
+    'NumberFormat': {
+      'locales': 'fr',
+      'extensionKeys': {
+        'nu': {'name': 'numberingSytsem', 'values': ['arab']},
+      },
+      'options': {
+        'style': {'values': ['decimal', 'currency', 'percent'], 'optionsBase': [{}, {'currency': 'EUR'}, {}]},
+        'currency': {'values': ['EUR', 'CNY', 'USD'], 'optionsBase': {'style': 'currency'}},
+        'currencyDisplay': {'values': ['symbol', 'code', 'name'], 'optionsBase': {'style': 'currency', 'currency': 'EUR'}},
+        'useGrouping': {'values': [true, false]},
+        'minimumIntegerDigits': {'values': [1, 2]},
+        'minimumFractionDigits': {'values': [1, 2, 3]},
+        'maximumFractionDigits': {'values': [1, 2, 3]},
+        'minimumSignificantDigits': {'values': [1, 2, 3]},
+        'maximumSignificantDigits': {'values': [1, 2, 3]},
 
-        const results = {
-          extensionKeys: {},
-          options: {}
-        };
-
-        for (const key in tests.DateTimeFormat.extensionKeys) {
-          const value = tests.DateTimeFormat.extensionKeys[key].values[0];
-          const name = tests.DateTimeFormat.extensionKeys[key].name;
-          const extKey = { [key]: value };
-          const dtf = new Intl.DateTimeFormat(
-            constructLocaleString('en-US', extKey)
-          );
-          results.extensionKeys[key] = dtf.resolvedOptions()[name] === value;
-        };
-
-        for (const name in tests.DateTimeFormat.options) {
-          const values = tests.DateTimeFormat.options[name].values;
-          const optionsBase = tests.DateTimeFormat.options[name].optionsBase || {};
-
-          const res = {};
-
-          for (const val of values) {
-            const options = Object.assign({
-              [name]: val
-            }, optionsBase);
-            const dtf = new Intl.DateTimeFormat('en-US', options);
-            res[val] = dtf.resolvedOptions()[name] === val;
-          };
-
-          if (Object.keys(res).length === 1) {
-            results.options[name] = res[Object.keys(res)[0]];
-          } else {
-            results.options[name] = res;
-          }
-        }
-        return results;
-      }
-    }
+        //'localeMatcher': {'values': ['lookup', 'best fit']},
+      },
+    },
+    'Collator': {
+      'locales': 'fr',
+      'extensionKeys': {
+        'co': {'name': 'collation', 'values': ['phonetic']},
+        'kn': {'name': 'numeric', 'values': [true]},
+        'kf': {'name': 'caseFirst', 'values': ['upper']},
+      },
+      'options': {
+        'usage': {'values': ['sort', 'search']},
+        'sensitivity': {'values': ['base', 'accent', 'case', 'variant']},
+        'ignorePunctuation': {'values': [true, false]},
+        'numeric': {'values': [true, false]},
+        'caseFirst': {'values': ['upper', 'lower', 'false']},
+      },
+    },
   }
 
 
@@ -83,7 +136,7 @@
     const results = {};
 
     for (const key in tests) {
-      results[key] = tests[key].test(Intl);
+      results[key] = testFormatter(key, Intl);
     };
 
     return results;
